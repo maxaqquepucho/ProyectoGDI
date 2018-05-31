8d61f490065701432c3e47664b3f46ba16fd2f0a
percyavaur <38476784+percyavaur@users.noreply.github.com>
2018-05-31 16:21:30 -0500

    controler modificado

--- src/proyectogdi/FXMLDocumentController.java
+++ src/proyectogdi/FXMLDocumentController.java
@@ -24,7 +24,9 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
-        label.setText("Hello World!");
+        System.out.println("sout    "
+                + "adfasdasda");
+        System.out.println("marcelo se la come");
     }
     
     @Override
